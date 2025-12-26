142de243ecc90b23014e2119f48ff21d89c2535f
Code3rr0r <maxbohanan880@gmail.com>
2025-12-25 18:41:25 -0600

    Update build script for FloppyOS v2.0

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-set -e
-
 nasm boot.asm -f bin -o boot.bin
 nasm kernel.asm -f bin -o kernel.bin
-
 cat boot.bin kernel.bin > os.img
-
-echo "FloppyOS build complete"
+echo "FloppyOS v2.0 built!"
