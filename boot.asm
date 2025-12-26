c18dcf7ef3c8cce231ee74898970188241079b62
Code3rr0r <maxbohanan880@gmail.com>
2025-12-25 18:43:18 -0600

    Modify boot.asm to load kernel sectors 2-11
    Updated boot.asm to load multiple kernel sectors and set segment registers.

--- boot.asm
+++ boot.asm
@@ -2,16 +2,22 @@ org 0x7C00
 bits 16
 
 start:
-    ; Load kernel (sector 2) into memory at 0x1000
-    mov bx, 0x1000
-    mov ah, 0x02
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
+    cli
+    xor ax,ax
+    mov ds,ax
+    mov es,ax
+
+    ; Load kernel: sectors 2-11 (adjust if kernel bigger)
+    mov bx,0x1000       ; load address
+    mov ah,0x02         ; BIOS read sector
+    mov al,10           ; number of sectors
+    mov ch,0
+    mov cl,2
+    mov dh,0
+    mov dl,0x00         ; floppy A:
     int 0x13
 
-    jmp 0x1000
+    jmp 0x1000:0
 
 times 510-($-$$) db 0
 dw 0xAA55
