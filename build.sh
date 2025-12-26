5a1f2c0c4b124e02bc841289f4736c2d1d9fa219
Code3rr0r <maxbohanan880@gmail.com>
2025-12-25 18:44:17 -0600

    Update build script to write OS image to floppy
    Added command to write the OS image to a floppy disk.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+# Build
 nasm boot.asm -f bin -o boot.bin
 nasm kernel.asm -f bin -o kernel.bin
 cat boot.bin kernel.bin > os.img
-echo "FloppyOS v2.0 built!"
+
+# Write to floppy
+sudo dd if=os.img of=/dev/fd0 bs=512 conv=notrunc
